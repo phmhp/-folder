d03b507ae918e2666767e3aab41dff9bdb9eb5cd
phmhp <phm1544@naver.com>
2022-09-13 16:57:41 +0900

    modification

--- main.c
+++ main.c
@@ -4,7 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello Sookmyung Electronics!\n");
+ 	int a, b;
+	 a=10;
+	 b=20;
+	 printf("%d + %d = %d\n", a, b, a+b) ;
 	return 0;
-}//f9 컴파일 단축키 /  실행 단축키는 f10   / 컴파일+실행은 f11
- 
+}
+  
