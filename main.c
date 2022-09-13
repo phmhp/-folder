1299d5c1c907f79fe50f299261e9f0e53c41d973
phmhp <phm1544@naver.com>
2022-09-13 16:49:49 +0900

    initial

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Hello Sookmyung Electronics!\n");
+	return 0;
+}//f9 컴파일 단축키 /  실행 단축키는 f10   / 컴파일+실행은 f11
+ 
