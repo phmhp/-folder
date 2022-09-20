874d4f3a69d7922add6c7e7dc7448fc12fe67d2c
phmhp <phm1544@naver.com>
2022-09-20 17:08:35 +0900

    02

--- main.c
+++ main.c
@@ -4,17 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int input_int;
-	float input_float;
+	char text;
+	int i;
 	
+	printf("input a number : ");
+	scanf("%c", &text);
 	
-	printf("enter an integer : ");
-	scanf("%d", &input_int) ;
+	i=text-'0';
+	printf("The input number is %i\n",i);
 	
-	printf("enter a float : ");
-	scanf("%f", &input_float);
-	
-	printf("integer : %d, float : %f\n", input_int, input_float);
 	
 	return 0;
 }
