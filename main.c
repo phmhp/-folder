969d008a000396f62eb6edaac79384cbe1d002f2
phmhp <phm1544@naver.com>
2022-10-05 18:47:55 +0900

    06

--- main.c
+++ main.c
@@ -5,70 +5,41 @@
 
 int main(int argc, char *argv[]) {
 	
-	int x,y; //두개의 입력정수 
-	char op;//연산자문자 
-	int result;//연산결과 저장 
+	int answer = 59; //정답 임의로 정함.
+	int x; //입력받는 내용을 저장할 변수
+	int trial =0; 
+	 
+	
+	do 
+	{ 
+	printf("Guess a number : ");	 //입력해라 문구 출력
+	scanf("%i",&x);	 //입력을 받음
+		trial=trial+1;
 	
 	
+		if (x> answer)
+			{
+			printf("high!\n");	 //입력 숫자가 정답보다 큰지 작은지 출력
+		}
+		else if (x<answer)
+		{
+	
+			printf("low!\n");
+		} 
+		else if (x=answer)
+		{
+			printf("Congratulation! ");
+		}
+	}	
+	while(x!=answer);
+	 //정답과 같은지 여부 판단
+	 
+	 
+	 printf(" trials : %i\n", trial); //시도 횟수 출력
+	 
+	 return 0;  
 
-	printf("enter the calculation : ");
-	scanf("%d %c %d",&x,&op,&y);
-	
-	
-	
-	switch(op)//op에 따라 케이스가 나뉨
-	
-	{
-		case '+':
-			result =x+y;
-			break;
-			
-		case '-':
-			result =x-y;
-			break;
-			
-		case '*':
-			result =x*y;
-			break;
-			
-		case '/':
-			result =x/y;
-			break;
-			
-			
-	 } 
-	
 
-	/*if (op =='+') 
-	{
-		result = x+y;
-	} 
-	
-	else if (op =='-')
-	{
-		result = x-y;
-	} 
-	
-	else if (op =='*')
-	{
-		result = x*y;
-	} 
-	
-	else if (op =='/')
-	{
-		result = x/y;
-	} 
-	*/
-	
-	printf("=%d", result);
-	return 0;
-	
 }
 
-
-//for 문을 활용해서 1부터 x까지 sum에 더하는 ㅗ드
-//for ()//반복(초기식;조건식;증감식)
-    	//더하기;
-	
-//printf(""); //결과 출력) 
 
