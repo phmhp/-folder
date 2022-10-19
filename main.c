e650ff340617f3f5f37d283fbb8ace898c69d9e3
phmhp <phm1544@naver.com>
2022-10-19 10:08:12 +0900

    02

--- main.c
+++ main.c
@@ -3,15 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int flag=1;
-	int y; //y를 main의 지역변수로 만들어주면 컴파일에러 발생안함. 
-	while (flag!=0)
+int main(int argc, char *argv[]) 
+{
+	int i;
+	for (i=0;i<5;i++)
 	{
-
-		y=3;
-		flag=0;
+		int temp=1;
+		printf("temp= %d\n",temp); //temp=1 만 5번 반복 출력됨.  
+		temp++;
 	}
-	y=4;
 	return 0;
 }
