fa68be4349d537eafc44dc2b9c9ccdf9decef2f2
phmhp <phm1544@naver.com>
2022-11-01 16:52:51 +0900

    03_2

--- main.c
+++ main.c
@@ -7,22 +7,26 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
-	int grade[SIZE];
-	int score[SIZE];
 	
+		int i;
+	int a[SIZE]={1,2,3,4,5};
+	int b[SIZE]={1,2,3,4,5};
+	int flag=0;
 	
 	for (i=0;i<SIZE;i++){
 		
-		grade[i]=rand() % 100;
+		if 	(a[i]!=b[i]){
+			
+			flag=1;
+			printf("a is not the same with b.\n");
+			break;
+		}	
+	
 	}
-	
-	for (i=0;i<SIZE;i++){
-		score[i]=grade[i];
-	}
-		
-	
-	
-	for (i=0;i<SIZE;i++)
-	printf ("score[%d] = %d\ngrade[%d] = %d\n",i,score[i],i,grade[i]);
+	if (flag==0)
+		printf("a is the same with b.\n");
 	return 0; 	}
+	
+	
+	
+
