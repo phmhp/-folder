cfb06b88bc751754e6724cb16653afc57198d0f6
phmhp <phm1544@naver.com>
2022-11-08 16:52:59 +0900

    03

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-
-	char str[4];
-	str[0]='a';
-	str[1]='b';		
-	str[2]='c';
-	str[3]='\0'; //NULL문자까지 넣어야함. 
+void main(void) {
+	char src[]="The worst things to eat before you sleep";
+	char dst[100];
 	
-	printf("%s\n",str); 
+	strcpy(dst,src);
+	
+	printf("copied string : %s\n",dst); 
+
+	return 0;
 
 }
