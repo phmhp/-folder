6472f2f8001867b7059cc7cffdee17fabe6d9536
phmhp <phm1544@naver.com>
2022-11-15 19:52:54 +0900

    07

--- main.c
+++ main.c
@@ -3,18 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-int main(int argc, char *argv[]) {
-	int i=100;
-	int *p =&i;
-	int **q =&p;
+char *proverb="All that glisters is not gold.";
+void setPointer(char **q)
+{
+	*q=proverb;
+}
+int main(void)
+{
+	char *p="zzz";
+	setPointer(&p);
+	printf("%s \n",p);
 	
-	*p =200;
-	printf("i =%d, *p =%d, **q =%d\n",i, *p, **q);
-	//포인터 *p에 따라서 값 모두 200됨. 
-	**q =300;
-	printf("i =%d, *p =%d, **q =%d\n",i, *p, **q);
-	
-	//이중포인터인 **q에 따라 값이 모두 300으로 변함. 
 	return 0;
 }
