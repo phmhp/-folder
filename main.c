80aed2434c19b7f6c388307fb8ffcbf433d553be
phmhp <phm1544@naver.com>
2022-11-30 20:35:02 +0900

    02

--- main.c
+++ main.c
@@ -1,22 +1,39 @@
 #include <stdlib.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-struct student {
-	int ID;
-	char name[20];
-	double grade;		
+struct point {
+	int x;
+	int y;	
 	};
 	
 	
 int main(int argc, char *argv[]) {
-	struct student s1={1003,"JuyeopKim",4.3};  //instance 
+	struct point p1,p2;
+	int xdiff, ydiff;
+	double dist;
 	
-	s1.ID=123456;
-	strcpy(s1.name,"Juyeop");
-	s1.grade=4.2;
+	printf("input p1 coordinate (x y) :");
+	scanf("%d %d",&p1.x,&p1.y);
 	
-	printf("ID : %d\n",s1.ID);
-	printf("name : %s\n", s1.name);
-	printf("grade : %.6f",s1.grade); 
+	printf("input p2 coordinate (x y) :");
+	scanf("%d %d",&p2.x,&p2.y);
+	
+	//p1,p2 간의 거리를 계산하고 출력
+	//(math.h의 sqrt()함수 활용
+	
+	//calculate xdiff
+	xdiff =p2.x-p1.x;
+	
+	//calculate ydiff
+	ydiff = p2.y-p1.y;
+	
+	//피타고라스의 정리 sqrt(xdiff*xdiff + ydiff*ydiff) =>dist 
+	dist = sqrt(xdiff*xdiff +ydiff*ydiff);
+
+
+	printf("distance : %lf\n",dist);
+	
 	return 0;
+	
 }
