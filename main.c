567aac26bde66642a59380d977232788631a6811
phmhp <phm1544@naver.com>
2022-12-06 16:31:51 +0900

    02

--- main.c
+++ main.c
@@ -2,23 +2,29 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct  Book{
+	int number;
+	char title[10];
+};
 
-int main(int argc, char *argv[]) {
-	char *pc =NULL ; 
-	int i =0;
+
+void main(int argc, char *argv[]) {
+	struct Book *p;
+		
+	p=(struct Book*)malloc(2*sizeof(struct Book)); //Book구조체 2개만큼 동적할당 => 2*sizeof(struct Book) 
 	
-	pc=(char*)malloc(100*sizeof(char));
-	if (pc==NULL){
+	if (p==NULL){
 		printf("메모리 할당 오류\n");
-		exit(1);
-	} 
-	
-	for (i=0;i<26;i++){
-		pc[i]='a'+i;
+		return; 
 	}
-	pc[i] =0;
-	printf("%s\n",pc);
-	free(pc);
+	
+	p->number =1;
+	strcpy(p->title,"C Programming");
+	
+	(p+1)->number =2;
+	strcpy((p+1)->title,"Electronics");
+	
+	free(p);
 	
 	return 0;
 }
